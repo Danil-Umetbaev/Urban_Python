171bf143ecc6cdfbf30af5d6751fed266562f6df
Danil-Umetbaev <hasagi84@mail.ru>
2024-10-09 12:21:26 +0500

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,20 @@
+def pick_up_password(number):
+    passwords = ""
+    for i in range(1, first_number):
+        for j in range(1, first_number):
+            if first_number % (i + j) == 0 and i != j and not(is_dublicate(passwords, i, j)):
+                passwords += str(i) + str(j)
+    return passwords
+
+def is_dublicate(passwords, num1, num2):
+    password = str(num2) + str(num1)
+    for i in range(0, len(passwords)-1, 2):
+        if password == passwords[i]+passwords[i+1]:
+            return True
+    return False
+
+first_number = 3
+while first_number <= 20:
+    passwords = pick_up_password(first_number)
+    print(first_number, ':', passwords)
+    first_number += 1

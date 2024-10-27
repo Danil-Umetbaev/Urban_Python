1b5ac93868ed3a181d6682867706a124a2bb926d
Danil-Umetbaev <hasagi84@mail.ru>
2024-10-27 19:16:50 +0500

    Add files via upload

---
+++ module_13.3.py
@@ -0,0 +1,17 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+API = ''
+bot = Bot(token=API)
+dispatcher = Dispatcher(bot, storage=MemoryStorage())
+
+@dispatcher.message_handler(commands='start')
+async def start(message):
+    await message.answer("Привет! Я бот, помогающий твоему здоровью.")
+
+@dispatcher.message_handler()
+async def all_messages(message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+if __name__ == '__main__':
+    executor.start_polling(dispatcher, skip_updates=True)

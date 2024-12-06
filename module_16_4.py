a8ea166cd560769af3cc097c73c459efe9159fd3
Danil-Umetbaev <hasagi84@mail.ru>
2024-12-06 13:02:50 +0500

    module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -33,14 +33,12 @@ async def update_user(user_id: int, username: str, age: int) -> str:
     for i in range(len(users)):
         if users[i].id == user_id:
             users[i] = User(id=user_id, username=username, age=age)
-            return str(User) + " was updated!"
+            return f'{str(users[i])} was updated!'
     raise HTTPException(status_code=404, detail="Такого пользоваетля нет(")
 
 @app.delete("/user/{user_id}")
 async def delete_message(user_id: int) -> str:
     for i in range(len(users)):
         if users[i].id == user_id:
-            del users[i]
-            return f'User id {user_id} was deleted!'
-
+            return f'{str(users.pop(i))} was deleted!'
     raise HTTPException(status_code=404, detail="Такого пользоваетля нет(")

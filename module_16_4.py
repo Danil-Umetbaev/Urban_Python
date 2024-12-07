061c0b6d33dd8958c294523fa925617cb7bd7d1a
Danil-Umetbaev <hasagi84@mail.ru>
2024-12-07 16:02:14 +0500

    Update module_16_4.py

--- module_16_4.py
+++ module_16_4.py
@@ -1,9 +1,8 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, status
 from typing import Annotated, List
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError, validator
 
 app = FastAPI()
-
 users = []
 
 class User(BaseModel):
@@ -14,31 +13,47 @@ class User(BaseModel):
     def __str__(self):
         return f'User {self.id}, username: {self.username}, age: {self.age}'
 
+
 @app.get("/users")
 async def get_all_users() -> List[User]:
     return users
 
+
 @app.post("/user/{username}/{age}")
-async def append_user(username: str, age: int) -> str:
+async def append_user(
+    username: Annotated[str, validator(min_length=3, max_length=20)],
+    age: Annotated[int, validator(min_value=0, max_value=120)],
+):
     new_id = max((user.id for user in users), default=0) + 1
-    new_user = User(id=new_id, username=username, age=age)
-    users.append(new_user)
-    return str(new_user) + " was created!"
+    try:
+        new_user = User(id=new_id, username=username, age=age)
+        users.append(new_user)
+        return {"message": f"{new_user} was created!", "user": new_user}
+    except ValidationError as e:
+        raise HTTPException(status_code=422, detail=str(e))
 
 
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: int, username: str, age: int) -> str:
+async def update_user(
+    user_id: int,
+    username: Annotated[str, validator(min_length=3, max_length=20)],
+    age: Annotated[int, validator(min_value=0, max_value=120)],
+):
+    try:
+        for i, u in enumerate(users):
+            if u.id == user_id:
+                users[i] = User(id=user_id, username=username, age=age)
+                return {"message": f"{users[i]} was updated!", "user": users[i]}
+        raise HTTPException(status_code=404, detail="User not found")
+    except ValidationError as e:
+        raise HTTPException(status_code=422, detail=str(e))
 
-    for i in range(len(users)):
-        if users[i].id == user_id:
-            users[i] = User(id=user_id, username=username, age=age)
-            return f'{str(users[i])} was updated!'
-    raise HTTPException(status_code=404, detail="Такого пользоваетля нет(")
 
 @app.delete("/user/{user_id}")
-async def delete_message(user_id: int) -> str:
-    for i in range(len(users)):
-        if users[i].id == user_id:
-            return f'{str(users.pop(i))} was deleted!'
-    raise HTTPException(status_code=404, detail="Такого пользоваетля нет(")
+async def delete_user(user_id: int):
+    for i, u in enumerate(users):
+        if u.id == user_id:
+            deleted_user = users.pop(i)
+            return {"message": f"{deleted_user} was deleted!"}
+    raise HTTPException(status_code=404, detail="User not found")
